ea1448901b6e7fca2ef5faf0106f4f8abf039cb0
matias.semelman <matias.semelman@devify.it>
2022-10-31 21:56:52 -0600

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
     <div className="App min-h-screen bg-background text-white">
       <div className="mx-[50px] ">
         <header className="flex justify-between align-middle h-[70px] sm:h-[100px] py-[10px]">
+          <div className="text-center">
+            <h1 className="text-orange">Matias Semelman</h1>
+            <smal>Software Dev.</smal>
+          </div>
         </header>
       </div>
     </div>
