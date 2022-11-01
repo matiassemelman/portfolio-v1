e54f6829f8e87fea1296f3ce88f63a0cbb73bbd9
matias.semelman <matias.semelman@devify.it>
2022-10-31 21:57:08 -0600

    Nav and resume download

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,17 @@ function App() {
             <h1 className="text-orange">Matias Semelman</h1>
             <smal>Software Dev.</smal>
           </div>
+          <nav className="hidden sm:flex">
+            <ul className="flex">
+              <li>About</li>
+              <li>Experience</li>
+              <li>Work</li>
+              <li>Contact</li>
+            </ul>
+            <div className="ml-[24px]">
+              <a href="/">Resume</a>
+            </div>
+          </nav>
         </header>
       </div>
     </div>
