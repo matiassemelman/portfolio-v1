65e81dccdbd07872046b8491a1aa5cf92d2c659f
matias.semelman <matias.semelman@devify.it>
2022-10-31 21:56:37 -0600

    Header flex, height and margins

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App min-h-screen bg-background text-white">
       <div className="mx-[50px] ">
+        <header className="flex justify-between align-middle h-[70px] sm:h-[100px] py-[10px]">
+        </header>
       </div>
     </div>
   );
