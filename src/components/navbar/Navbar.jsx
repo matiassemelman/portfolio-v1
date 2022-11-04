0ef21bb134c3d88568f04cf586ed76df4bb67ad7
matias.semelman <matias.semelman@devify.it>
2022-11-03 22:22:44 -0600

    Smartphone view test

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,15 +16,17 @@ export const Navbar = () => {
         </div>
         
         <Menu className='sm:hidden' size={'48px'} onClick={handleOpen}/>
-        <nav className={`sm:flex ${isNavbarOpen ? 'visible' : 'hidden'}`}>
-          <ul className="flex">
-            <li>About</li>
-            <li>Experience</li>
-            <li>Work</li>
-            <li>Contact</li>
+        <nav className={`sm:flex ${isNavbarOpen ? 'visible absolute top-20 left-40' : 'hidden'}`}>
+          <ul className="sm:flex">
+            <li className='hidden sm:px-2'>About</li>
+            <li className='hidden sm:px-2'>Experience</li>
+            <li className='hidden sm:px-2'>Work</li>
+            <li className='hidden sm:px-2'>Contact</li>
           </ul>
-          <div className="ml-[24px]">
-            <a href="/">Resume</a>
+          <div className="text-3xl mr-2">
+            <a href="/">{isNavbarOpen && '💾' }Resume</a>
+            <div className='my-2'></div>  
+            <a href="http://wa.me/5491130332808" target="_blank" rel='noreferrer'>Whatsapp</a>
           </div>
         </nav>
       </header>
