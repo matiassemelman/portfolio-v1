1fee159bb25684eaf3986906374f6501af0552ac
matias.semelman <matias.semelman@devify.it>
2022-12-17 22:08:09 -0300

    Responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react';
+import {AiOutlineClose, AiOutlineMenu, AiOutlineWhatsApp} from 'react-icons/ai'
+
 export const Navbar = () => {
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <header className="flex justify-between align-middle h-[70px] sm:h-[100px] py-[10px]">
       <div className="text-center">
-        <h1 className="text-orange">Matias Semelman</h1>
-        <smal>Software Dev.</smal>
+        <h1 className="text-orange font-bold text-2xl">Matias Semelman</h1>
+        <small className='text-lg'>Software Dev.</small>
+      </div>
+      <nav>
+        <ul className='p-2 uppercase hidden md:flex'>
+          <li className='p-2'><a href="/">Home</a></li>
+          <li className='p-2'><a href="/">Blog</a></li>
+          <li className='p-2'><a href="/">Projects</a></li>
+          <li className='p-2'><a href="/">Contact</a></li>
+          <li className='p-2'><a target="_blank" href="/wa.me/5491130332808"><AiOutlineWhatsApp/></a></li>
+        </ul>
+      </nav>
+      <nav className={!nav ? 'fixed left-0 top-0 w-[50%] h-full border-r border-r-orange bg-orange ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <ul className='p-4 uppercase'>
+          <li className='p-4 border-b border-b-black text-bold'><a href="/">Home</a></li>
+          <li className='p-4 border-b border-b-black text-bold'><a href="/">Blog</a></li>
+          <li className='p-4 border-b border-b-black text-bold'><a href="/">Projects</a></li>
+          <li className='p-4 border-b border-b-black text-bold'><a href="/">Contact</a></li>
+          
+        </ul>
+      </nav>
+      <div className="hidden ">
+        <button>
+          Current
+          <ul>
+            <li>Light</li>
+            <li>Dark</li>
+          </ul>
+        </button>
+      </div>
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={40} className="mt-2"/> : <AiOutlineMenu size={40} className="mt-2"/>}
+      
       </div>
     </header>
   );
