8ccc6bf0fd7dd9c718703bb4d2f4530dd688e33c
matias.semelman <matias.semelman@devify.it>
2022-12-19 19:26:52 -0300

    Responsive design applied

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import {AiOutlineClose, AiOutlineMenu, AiOutlineWhatsApp} from 'react-icons/ai'
 
 export const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(true)
 
   const handleNav = () => {
     setNav(!nav)
@@ -11,7 +11,7 @@ export const Navbar = () => {
   return (
     <header className="flex justify-between align-middle h-[70px] sm:h-[100px] py-[10px]">
       <div className="text-center">
-        <h1 className="text-orange font-bold text-2xl">Matias Semelman</h1>
+        <h1 className="text-green font-bold text-2xl">Matias Semelman.</h1>
         <small className='text-lg'>Software Dev.</small>
       </div>
       <nav>
@@ -20,10 +20,10 @@ export const Navbar = () => {
           <li className='p-2'><a href="/">Blog</a></li>
           <li className='p-2'><a href="/">Projects</a></li>
           <li className='p-2'><a href="/">Contact</a></li>
-          <li className='p-2'><a target="_blank" href="/wa.me/5491130332808"><AiOutlineWhatsApp/></a></li>
+          <li className='pt-3 px-2'><a target="_blank" href="http://wa.me/5491130332808"><AiOutlineWhatsApp className='text-green'/></a></li>
         </ul>
       </nav>
-      <nav className={!nav ? 'fixed left-0 top-0 w-[50%] h-full border-r border-r-orange bg-orange ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <nav className={!nav ? 'fixed left-0 top-0 w-[50%] h-full border-r border-r-green bg-green ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <ul className='p-4 uppercase'>
           <li className='p-4 border-b border-b-black text-bold'><a href="/">Home</a></li>
           <li className='p-4 border-b border-b-black text-bold'><a href="/">Blog</a></li>
@@ -43,7 +43,6 @@ export const Navbar = () => {
       </div>
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={40} className="mt-2"/> : <AiOutlineMenu size={40} className="mt-2"/>}
-      
       </div>
     </header>
   );
