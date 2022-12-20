abbcbe4243749314fa641bef80475bc3e93a43e6
matias.semelman <matias.semelman@devify.it>
2022-12-19 22:54:46 -0300

    Image without background

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,13 @@
 export const Hero = () => {
-    return (
-        <div>
-            <div >
-        <img src="https://imgur.com/DQ2vlAX" alt="Avatar of myself created by Midjourney bot" />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <img
+          src="https://i.imgur.com/N7nem8n.png"
+          alt="Avatar of myself created by Midjourney bot"
+          className=""
+        />
+      </div>
+    </div>
+  );
+};
