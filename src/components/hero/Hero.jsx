5bedcc09fc0aa60366f2480636ab82737b27d587
matias.semelman <matias.semelman@devify.it>
2022-12-19 22:48:30 -0300

    Initial Commit

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,9 @@
+export const Hero = () => {
+    return (
+        <div>
+            <div >
+        <img src="https://imgur.com/DQ2vlAX" alt="Avatar of myself created by Midjourney bot" />
+            </div>
+        </div>
+    )
+}
