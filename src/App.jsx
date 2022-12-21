f548ec7c30a6459a5b342e0eeb8bd06bc3dce680
matias.semelman <matias.semelman@devify.it>
2022-12-20 22:50:14 -0300

    Add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
 
 function App() {
   
 
   return (
-    <div className="App min-h-screen bg-background" >
+    <div className="App min-h-screen bg-background  " >
       <div className="mx-[50px] ">
         <Navbar/>
+        <Hero/>
       </div>
     </div>
   );
